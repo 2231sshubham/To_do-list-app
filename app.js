82eed7c785e5ecd8dccf1dd70f99d66e417ddfe5
2231sshubham <2231sshubham@gmail.com>
2021-10-15 15:15:59 +0530

    c

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+// const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./config")
 const date = require(__dirname + "/date.js");
+
+
+name = config.name;
+pass = config.pass;
+var url = "mongodb+srv://" + name + ":" + pass + "@email-editor.kre0j.mongodb.net/to-do-list";
+mongoose.connect(url)
+
+var schema = new mongoose.Schema({
+  loc : String,
+  task : String
+});
+
+var Task = mongoose.model('Task',schema);
+
+
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -12,30 +30,60 @@ app.set('view engine','ejs');
 let t = ['Buy Food','Cook Food','Eat Food'];
 let workItems = [];
 
-app.get("/",function(req,res){
+app.get("/",async function(req,res){
 
   let today = date.getDate();
-  res.render('index', {listTitle:today,item:t});
+  const arr = await Task.find({loc:"Home"},{task:1,_id:0});
+  let task = []
 
-})
+  for(let i=0;i<arr.length;i++){
+      task.push(arr[i].task);
+  };
+  res.render('index', {listTitle:today,item:task});
+
+});
+
 
 
 app.post("/",function(req,res){
   let new_t = req.body.nt;
   if(req.body.ls==="Work"){
     workItems.push(new_t);
+    let obj = new Task({
+      loc:"Work",
+      task:new_t
+    });
+
+    obj.save();
     res.redirect("/work");
   }
   else{
   t.push(new_t);
+  let obj = new Task({
+    loc:"Home",
+    task:new_t
+  });
+
+  obj.save();
   res.redirect("/");
   }
 
 })
 
+app.post("/del",async function(req,res){
+  const val = req.body.checkbox;
+  let del = await Task.deleteOne({task:val});
+  res.redirect(req.headers.origin)
+})
 
-app.get("/work",function(req,res){
-  res.render("index", {listTitle:"Work List",item:workItems});
+app.get("/work",async function(req,res){
+  const arr = await Task.find({loc:"Work"},{task:1,_id:0});
+  let task = []
+
+  for(let i=0;i<arr.length;i++){
+      task.push(arr[i].task);
+  };
+  res.render("index", {listTitle:"Work List",item:task});
 })
 
 app.get("/about",function(req,res){
